e561e52d5261839d5d956878e048814cf37185d7
Thomas Enggaard Christensen <thomas@enggaard.net>
2022-09-06 20:17:53 +0200

    Cleaned out old tradeskill code Updated AceAddon files to wotlk

---
+++ OldCode.lua
@@ -0,0 +1,55 @@
+function DanesOfHonor:CreateExportString()
+    local exportString = '{\"version\":' .. version .. ',\"data\":{'
+
+    for realm, players in pairs(DOHGADB) do
+        if (realm ~= "profileKeys") then
+            exportString = exportString .. '\"' .. realm .. '\": {'
+            for player, data in pairs(players) do
+                exportString = exportString .. '\"' .. player .. '\": {'
+                exportString = exportString .. '\"faction\": \"' ..
+                                   data["faction"] .. '\",'
+                exportString = exportString .. '\"class\": \"' ..
+                                   FixNil(data["class"]) .. '\",'
+
+                if (data["level"]) then
+                    exportString = exportString .. '\"level\": ' ..
+                                       data["level"] .. ','
+                end
+
+                if (data["guildName"]) then
+                    exportString = exportString .. '\"guildName\": \"' ..
+                                       data["guildName"] .. '\",'
+                    exportString = exportString .. '\"guildRankName\": \"' ..
+                                       data["guildRankName"] .. '\",'
+                    exportString = exportString .. '\"guildRankIndex\": ' ..
+                                       data["guildRankIndex"] .. ','
+                end
+                exportString = exportString ..
+                                   DanesOfHonor:TradeskillsToJson(
+                                       data["tradeskills"])
+                -- other data types here
+                exportString = exportString .. '},'
+            end
+            exportString = exportString .. '},'
+        end
+    end
+    exportString = exportString .. '}}'
+
+    exportString = exportString:gsub(",}", "}")
+    exportString = exportString:gsub(",]", "]")
+    DanesOfHonor:DisplayExportString(exportString)
+
+end
+
+function DanesOfHonor:DisplayExportString(exportString)
+
+    local encoded = DanesOfHonor:encode(exportString)
+
+    DohFrame:Show()
+    DohFrameScroll:Show()
+    DohFrameScrollText:Show()
+    DohFrameScrollText:SetText(encoded)
+    DohFrameScrollText:HighlightText()
+
+    DohFrameButton:SetScript("OnClick", function(self) DohFrame:Hide() end)
+end
---
+++ Utils/Utils.lua
@@ -0,0 +1,8 @@
+function FixNil(x)
+    if (x == nil) then
+        return ''
+    else
+        return x;
+    end
+end
+
